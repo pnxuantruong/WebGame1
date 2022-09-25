717c22685ff697ac811d5c19244941adbd58e51a
Phạm Nguyễn Xuân Trường <76194393+pnxuantruong@users.noreply.github.com>
2022-09-25 23:14:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext("2d");
-    canvas.width = 800;
+    canvas.width = 1200;
     canvas.height = 720;
     // canvas.width = window.innerWidth;
     // canvas.height = window.innerHeight;
